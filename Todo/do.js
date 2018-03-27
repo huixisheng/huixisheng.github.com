9738b629722fa097ce424ea49434a40c48d1e267
huixisheng <huixisheng@huixishengdeMacBook-Pro.local>
2018-03-27 21:17:59 +0800

    Site updated: 2018-03-27 21:17:59

---
+++ Todo/do.js
@@ -0,0 +1,8 @@
+var arr = ['huixisheng', 'js', 'css', 'webpack'];
+arr.forEach((value) => {
+  if (value == 'huixisheng') {
+    return  false;
+  }
+  console.log(value);
+});
+// http://www.gowhich.com/blog/649
